de0c7c2fb5c96a0ed655f7051ae1d5ee0356a1e0
马平凡 <mapingfan@gmail.com>
2018-05-06 13:51:05 +0800

    lambda并行计算

---
+++ src/D3/Ex01.java
@@ -0,0 +1,21 @@
+package D3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+/**
+ * Lambda表达式并行计算.
+ */
+public class Ex01 {
+
+    public static int add(List<Integer> values) {
+        values.parallelStream().forEach(value -> System.out.println(value));
+        return values.parallelStream().mapToInt(value -> value).sum();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 6);
+        System.out.println(add(list));
+    }
+}
